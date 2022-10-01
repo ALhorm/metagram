b45c679f055381dfb711462068ef755b6f2b413e
ALhorm <75273270+ALhorm@users.noreply.github.com>
2022-10-02 01:49:07 +0300

    02-10-22-01-49

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,3 @@
-import furo
-
-extensions = [
-  'sphinx.ext.autodoc',
-  'furo'
-]
-
-html_title = 'metagram'
-html_theme = 'furo'
+html_theme = 'sphinx_rtd_theme'
 html_logo = 'static/metagram-logo.png'
 html_static_path = ['static']
